6e9c75f73f256e7fbfd711ba6cf7208da2950967
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2020-02-06 14:38:32 +0100

    Split publications into own and other publications.

--- desdeo/publications/models.py
+++ desdeo/publications/models.py
@@ -15,5 +15,20 @@ class Publication(models.Model):
 
     def __str__(self):
         return self.title
+
+class OtherPublication(models.Model):
+    """Defines a bibliographical entry for publications outside the DESDEO project.
+
+    """
+    authors = models.TextField(max_length=200, help_text="List of authors")
+    title = models.TextField(max_length=200, help_text="Title of the publication")
+    year = models.IntegerField(default=2000, help_text="Publication year. Used for sorting. NOT DISPLAYED!")
+    type = models.CharField(max_length=100, default="Journal article", help_text="Type of publication, example: Journal article, conference paper")
+    info = models.TextField(help_text="Example: journal name, issue, pages, publication year")
+    doi_url = models.CharField(max_length=100, help_text="Link to publisher's webpage.")
+    open_access_url = models.CharField(max_length=100, help_text="Optional, link to open access source.", blank=True)
+
+    def __str__(self):
+        return self.title
         
 
--- desdeo/publications/views.py
+++ desdeo/publications/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
-from .models import Publication
+from .models import Publication, OtherPublication
 from frontpage.views import subpages_list
 
 def index(request):
     publication_list = Publication.objects.order_by("-year")
+    other_publication_list = OtherPublication.objects.order_by("-year")
     
     context = {
         "publication_list": publication_list,
+        "other_publication_list": other_publication_list,
         "subpages_list": subpages_list,
         "this_page": "publications",
     }
