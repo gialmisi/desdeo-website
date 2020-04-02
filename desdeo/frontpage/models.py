e4a6c5972e6370db78ee5ca1afbfb188c5b90cff
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2020-04-02 15:47:19 +0300

    Updated the help text of carousel image to include encourage the usage in png images if the image has transparent parts, like a transparent background.

--- desdeo/frontpage/models.py
+++ desdeo/frontpage/models.py
@@ -6,17 +6,25 @@ class CarouselImages(models.Model):
     """Contains the images to be displayed on the carousel.
 
     """
+
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000,
-                                   blank=True,
-                                   help_text="Caption text to be shown on the image."
+    description = models.TextField(
+        max_length=1000,
+        blank=True,
+        help_text="Caption text to be shown on the image.",
     )
     order = models.IntegerField(
-        help_text=("Order of the displayed image in the carousel in ascending order. Smaller is first.")
+        help_text=(
+            "Order of the displayed image in the carousel in ascending order. Smaller is first."
+        )
     )
-    image = StdImageField(upload_to="images/",
-                          variations={"large": {"width": 1200, "height": 600}},
-                          help_text=("Image should be 1200x600 pixels, or bigger.")
+    image = StdImageField(
+        upload_to="images/",
+        variations={"large": {"width": 1200, "height": 600}},
+        help_text=(
+            "Image should be 1200x600 pixels, or bigger. Use a png format if "
+            "the image has transparent sections."
+        ),
     )
 
     def __str__(self):
@@ -28,24 +36,29 @@ class Featurette(models.Model):
     the front page.
 
     """
+
     title = models.CharField(max_length=100)
-    text_content = models.TextField(max_length=1000,
-                                    help_text=("The text to be shown next to the featurette."),
+    text_content = models.TextField(
+        max_length=1000,
+        help_text=("The text to be shown next to the featurette."),
     )
-    link = models.CharField(max_length=100,
-                            help_text=("The link the button on the featurettes redirects to."),
+    link = models.CharField(
+        max_length=100,
+        help_text=("The link the button on the featurettes redirects to."),
     )
     order = models.IntegerField(
-        help_text=("Order of the displayed featurette in ascending order. Smaller is first.")
+        help_text=(
+            "Order of the displayed featurette in ascending order. Smaller is first."
+        )
     )
     image = models.ImageField(
         upload_to="images/",
-        help_text=("Image should be big, preferably bigger than 800 by 400 pixels, "
-                   "and should have an aspect ratio of 2:1. If any padding is added, "
-                   "it should be transparent. Use a png format.")
+        help_text=(
+            "Image should be big, preferably bigger than 800 by 400 pixels, "
+            "and should have an aspect ratio of 2:1. If any padding is added, "
+            "it should be transparent. Use a png format."
+        ),
     )
 
     def __str__(self):
         return self.title
-        
-        
