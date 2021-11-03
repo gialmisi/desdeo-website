99d197306d2d253f87e0c0dbc7e2364e2bbfaf5e
Giovanni Misitano <giovanni.misitano@jyu.fi>
2021-11-03 15:20:46 +0200

    Added help text to news model.

--- desdeo/news/models.py
+++ desdeo/news/models.py
@@ -11,7 +11,9 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     pub_date = models.DateField(help_text="The publication date.")
-    contents = models.TextField(help_text="The textual content of the news article.")
+    contents = models.TextField(
+        help_text="The textual content of the news article.  Markdown is supported. Image names should follow the path '/media/images/news/'. Example: '/media/images/news/figure_name.png'."
+    )
     image = models.ImageField(
         help_text="An image to be displayed at the end of the news article.", blank=True, upload_to="images/news"
     )
